5f450a8177f98265a52d8e5bba13d9bc18b763f2
Ryan LaHue <ryan@hasoffers.com>
2013-05-16 10:28:22 -0700

    Move javascript inside functions

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,243 +1,235 @@
-"use strict";
+(function(window, DISQUS, undefined) {
+    "use strict";
 
-/**
- * Controller for list of methods
- */
-function ControllerListCtrl($scope,
-                            $routeParams,
-                            Util) {
-
-    Util.getExternalDoc().success(function(controllers){
-        $scope.controllerList = Util.findMethodsByController(
-            controllers,
-            $routeParams.controllerName
-        );
-    });
-}
-
-/**
- * Controller for a list of sidebar with controllers and methods
- */
-function SideBarController($scope, Util) {
-    Util.getExternalDoc().success(function(controllers){
-        $scope.ctrlrs = Util.aggregateByController(controllers);
-        $scope.searchQuery = "";
-        $scope.isFiltering = false;
-        $scope.$watch('searchQuery', function(){
-            $scope.isFiltering = $scope.searchQuery.length === 0 ? false : true;
+    /**
+     * Controller for list of methods
+     */
+    window.ControllerListCtrl = function($scope, $routeParams, Util) {
+        Util.getExternalDoc().success(function(controllers){
+            $scope.controllerList = Util.findMethodsByController(
+                controllers,
+                $routeParams.controllerName
+            );
         });
-    });
-}
+    };
 
-/**
- * controls the disqus comments
- * resets everytime a new method view is displayed
- */
-function DisqusController($scope, $routeParams){
-   var identifier = $routeParams.controllerName + '::' + $routeParams.methodName;
-   var url = document.location.origin + '/#!' + identifier;
+    /**
+     * Controller for a list of sidebar with controllers and methods
+     */
+    window.SideBarController = function($scope, Util) {
+        Util.getExternalDoc().success(function(controllers){
+            $scope.ctrlrs = Util.aggregateByController(controllers);
+            $scope.searchQuery = "";
+            $scope.isFiltering = false;
+            $scope.$watch('searchQuery', function(){
+                $scope.isFiltering = $scope.searchQuery.length === 0 ? false : true;
+            });
+        });
+    };
 
-   // reset the disqus comments for the current page
-   DISQUS.reset({
-     reload: true,
-     config: function () {
-       this.page.identifier = identifier;
-       this.page.url = url;
-     }
-   });
-}
+    /**
+     * controls the disqus comments
+     * resets everytime a new method view is displayed
+     */
+    window.DisqusController = function($scope, $routeParams){
+       var identifier = $routeParams.controllerName + '::' + $routeParams.methodName;
+       var url = document.location.origin + '/#!' + identifier;
 
-/**
- * Method view controller
- */
-function MethodViewCtrl($scope,
-                  $routeParams,
-                  $http,
-                  Util,
-                  UserInfo) {
+       // reset the disqus comments for the current page
+       DISQUS.reset({
+         reload: true,
+         config: function () {
+           this.page.identifier = identifier;
+           this.page.url = url;
+         }
+       });
+    };
 
+    /**
+     * Method view controller
+     */
+    window.MethodViewCtrl = function($scope, $routeParams, $http, Util, UserInfo) {
+        Util.getExternalDoc().success(function(data){
+            var displayCtrl = Util.findMethod(data,$routeParams.controllerName,
+                                             $routeParams.methodName);
 
+            if (displayCtrl != null) {
+                Util.getModelDoc().success(function(model){
+                    $scope.displayCtrl = Util.bindFields(model, displayCtrl);
+                    $scope.apiParams   = Util.buildApiConstructor($scope.displayCtrl);
+                    // default to user info
+                    $scope.displayCtrl.networkToken = UserInfo.getProperty('NetworkToken');
+                    $scope.displayCtrl.networkId = UserInfo.getProperty('NetworkId');
+                    $scope.updateApiCall();
+                }); // get model doc
+            } // display ctrl not null
 
-    Util.getExternalDoc().success(function(data){
-        var displayCtrl = Util.findMethod(data,$routeParams.controllerName,
-                                         $routeParams.methodName);
+          /**
+           * displayRequired
+           * @param bool - is required
+           * @return string
+           */
+          $scope.displayRequired = function(isRequired) {
+              return true === isRequired ? '*' : '';
+          };
 
-        if (displayCtrl != null) {
-            Util.getModelDoc().success(function(model){
-                $scope.displayCtrl = Util.bindFields(model, displayCtrl);
-                $scope.apiParams   = Util.buildApiConstructor($scope.displayCtrl);
-                // default to user info
-                $scope.displayCtrl.networkToken = UserInfo.getProperty('NetworkToken');
-                $scope.displayCtrl.networkId = UserInfo.getProperty('NetworkId');
-                $scope.updateApiCall();
-            }); // get model doc
-        } // display ctrl not null
+          /**
+           * hideContain
+           * @return bool - whether there is a contains list
+           */
+          $scope.hideContain = function() {
+              return ($scope.displayCtrl && $scope.displayCtrl.containList == null);
+          };
 
-      /**
-       * displayRequired
-       * @param bool - is required
-       * @return string
-       */
-      $scope.displayRequired = function(isRequired) {
-          return true === isRequired ? '*' : '';
-      };
+          /**
+           * hideTrashButton
+           * @param object - param to check
+           * @return bool - whether or not to show the trash button for this parameter
+           */
+          $scope.hideTrashButton = function(param) {
+              return (param.value.isRequired);
+          };
 
-      /**
-       * hideContain
-       * @return bool - whether there is a contains list
-       */
-      $scope.hideContain = function() {
-          return ($scope.displayCtrl && $scope.displayCtrl.containList == null);
-      };
+          /**
+           * addFilterField
+           * @param array - reference array to modify
+           * @return void
+           */
+          $scope.addFilterField = function(addTo) {
+              addTo.push({});
+          };
 
-      /**
-       * hideTrashButton
-       * @param object - param to check
-       * @return bool - whether or not to show the trash button for this parameter
-       */
-      $scope.hideTrashButton = function(param) {
-          return (param.value.isRequired);
-      };
+          /**
+           * runApiCall
+           * runs the api call
+           * @return void
+           */
+          $scope.runApiCall = function() {
+              if ($scope.displayCtrl.networkToken == null ) {
+                  $scope.apiResponse = 'Please provide Network Token';
+                  return;
+              }
 
-      /**
-       * addFilterField
-       * @param array - reference array to modify
-       * @return void
-       */
-      $scope.addFilterField = function(addTo) {
-          addTo.push({});
-      };
+              // update user info
+              UserInfo.setProperty('NetworkToken', $scope.displayCtrl.networkToken);
 
-      /**
-       * runApiCall
-       * runs the api call
-       * @return void
-       */
-      $scope.runApiCall = function() {
-          if ($scope.displayCtrl.networkToken == null ) {
-              $scope.apiResponse = 'Please provide Network Token';
-              return;
-          }
+              if ($scope.displayCtrl.networkId == null ) {
+                  $scope.apiResponse = 'Please provide Network Id';
+                  return;
+              }
 
-          // update user info
-          UserInfo.setProperty('NetworkToken', $scope.displayCtrl.networkToken);
+              // update user info
+              UserInfo.setProperty('NetworkId', $scope.displayCtrl.networkId);
 
-          if ($scope.displayCtrl.networkId == null ) {
-              $scope.apiResponse = 'Please provide Network Id';
-              return;
-          }
+              $http.jsonp($scope.apiCall.replace("json", "jsonp") +
+                          "&callback=JSON_CALLBACK")
+                  .success(function(data) {
+                      $scope.apiResponse = angular.toJson(data, true);
+                  })
+                  .error(function(data) {
+                      $scope.apiResponse = angular.toJson(data, true);
+                  });
+          };
 
-          // update user info
-          UserInfo.setProperty('NetworkId', $scope.displayCtrl.networkId);
+          /**
+           * updateApiCall
+           * constructs a string representation of the api call
+           */
+          $scope.updateApiCall = function() {
 
-          $http.jsonp($scope.apiCall.replace("json", "jsonp") +
-                      "&callback=JSON_CALLBACK")
-              .success(function(data) {
-                  $scope.apiResponse = angular.toJson(data, true);
-              })
-              .error(function(data) {
-                  $scope.apiResponse = angular.toJson(data, true);
-              });
-      };
+              $scope.apiCall = "http://api.hasoffers.com/v3/" +
+                  $scope.displayCtrl.controllerName +
+                  ".json?Method=" + $scope.displayCtrl.methodName;
 
-      /**
-       * updateApiCall
-       * constructs a string representation of the api call
-       */
-      $scope.updateApiCall = function() {
+              if ($scope.displayCtrl.networkToken != null) {
+                  $scope.apiCall += "&NetworkToken=" + $scope.displayCtrl.networkToken;
+              }
 
-          $scope.apiCall = "http://api.hasoffers.com/v3/" +
-              $scope.displayCtrl.controllerName +
-              ".json?Method=" + $scope.displayCtrl.methodName;
+              if ($scope.displayCtrl.networkId != null) {
+                  $scope.apiCall += "&NetworkId=" + $scope.displayCtrl.networkId;
+              }
 
-          if ($scope.displayCtrl.networkToken != null) {
-              $scope.apiCall += "&NetworkToken=" + $scope.displayCtrl.networkToken;
-          }
+              angular.forEach($scope.apiParams, function(param) {
 
-          if ($scope.displayCtrl.networkId != null) {
-              $scope.apiCall += "&NetworkId=" + $scope.displayCtrl.networkId;
-          }
+                  var fieldType   = param.value.name;
+                  var parseValues = param.parse;
 
-          angular.forEach($scope.apiParams, function(param) {
-
-              var fieldType   = param.value.name;
-              var parseValues = param.parse;
-
-              switch (fieldType) {
-                  case "filters":
-                      // Default nesting to "AND"
-                      var nesting = "";
-                      if (param.nesting !== "AND") {
-                          nesting = "[OR]";
-                      }
-
-                      angular.forEach(parseValues, function(value) {
-
-                          // Default operator is equals
-                          var operator = "[]";
-                          if (value.selectOperator != null) {
-                              operator = "[" + value.selectOperator + "]";
+                  switch (fieldType) {
+                      case "filters":
+                          // Default nesting to "AND"
+                          var nesting = "";
+                          if (param.nesting !== "AND") {
+                              nesting = "[OR]";
                           }
 
-                          if (value.selectField != null &&
-                              value.selectValue != null) {
+                          angular.forEach(parseValues, function(value) {
 
+                              // Default operator is equals
+                              var operator = "[]";
+                              if (value.selectOperator != null) {
+                                  operator = "[" + value.selectOperator + "]";
+                              }
+
+                              if (value.selectField != null &&
+                                  value.selectValue != null) {
+
+                                  $scope.apiCall += "&" + fieldType +
+                                      nesting + "[" + value.selectField.name + "]" +
+                                      operator + "=" + value.selectValue;
+                              }
+                          });
+                          break;
+
+                      case "sort":
+                      case "data":
+                          angular.forEach(parseValues, function(value) {
+
+                              if (value.selectField != null &&
+                                  value.selectValue != null) {
+
+                                  $scope.apiCall += "&" + fieldType +
+                                      "[" + value.selectField.name + "]" +
+                                      "=" + value.selectValue;
+                              }
+                          });
+                          break;
+
+                      case "fields":
+                          angular.forEach(parseValues, function(value) {
+
+                              if (value.name != null) {
+                                  $scope.apiCall += "&" + fieldType +
+                                      "[]=" + value.name;
+                              }
+                          });
+                          break;
+
+                      case "field":
+                          if (parseValues.selectValue.name != null) {
                               $scope.apiCall += "&" + fieldType +
-                                  nesting + "[" + value.selectField.name + "]" +
-                                  operator + "=" + value.selectValue;
+                                  "=" + parseValues.selectValue.name;
                           }
-                      });
-                      break;
+                          break;
 
-                  case "sort":
-                  case "data":
-                      angular.forEach(parseValues, function(value) {
+                      case "contain":
+                          angular.forEach(parseValues, function(value) {
 
-                          if (value.selectField != null &&
-                              value.selectValue != null) {
+                              if (value.containName != null) {
+                                  $scope.apiCall += "&" + fieldType +
+                                      "[]=" + value.containName;
+                              }
+                          });
+                          break;
 
+                     default:
+                          if (parseValues.selectValue != null) {
                               $scope.apiCall += "&" + fieldType +
-                                  "[" + value.selectField.name + "]" +
-                                  "=" + value.selectValue;
+                                  "=" + parseValues.selectValue;
                           }
-                      });
-                      break;
-
-                  case "fields":
-                      angular.forEach(parseValues, function(value) {
-
-                          if (value.name != null) {
-                              $scope.apiCall += "&" + fieldType +
-                                  "[]=" + value.name;
-                          }
-                      });
-                      break;
-
-                  case "field":
-                      if (parseValues.selectValue.name != null) {
-                          $scope.apiCall += "&" + fieldType +
-                              "=" + parseValues.selectValue.name;
-                      }
-                      break;
-
-                  case "contain":
-                      angular.forEach(parseValues, function(value) {
-
-                          if (value.containName != null) {
-                              $scope.apiCall += "&" + fieldType +
-                                  "[]=" + value.containName;
-                          }
-                      });
-                      break;
-
-                 default:
-                      if (parseValues.selectValue != null) {
-                          $scope.apiCall += "&" + fieldType +
-                              "=" + parseValues.selectValue;
-                      }
-                      break;
-              } // switch
-          }); // foreach
-      }; // updateApiCall()
-    }); // get external doc
-} // MethodViewCtrl
+                          break;
+                  } // switch
+              }); // foreach
+          }; // updateApiCall()
+        }); // get external doc
+    }; // MethodViewCtrl
+})(window, DISQUS);
